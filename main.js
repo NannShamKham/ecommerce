df34aa2ec64d72699690e821b80a961301735957
NannShamKham <shamkham1123@gmail.com>
2022-05-24 23:18:02 +0630

    first

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 import './style.scss'
 import 'bootstrap'
+import VenoBox from "venobox";
 
+
+
+new VenoBox({
+    selector: '.venobox',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane',
+    navKeyboard:'true'
+});
